23635d8c9d569c677701a2a12be726b51a9d5a76
Ehis Edemakhiota <edemaehiz@gmail.com>
2022-05-26 15:59:24 +0100

    fixing build issues

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-let { init } = require("./data/db-config");
+let { init } = require("./src/helpers/db.config");
 let basicAuth = require("./src/helpers/basicAuth");
 let errorResponse = require("./errorHandler");
 init();
